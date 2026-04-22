bf99e5bde908d6b53079cc6494bc6bbde4034c52
Abhiram <b.abhi2277@gmail.com>
2026-04-23 01:18:05 +0530

    Update Index

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,11 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 import connectDB from './config/db.js';
+import adminRoutes from './routes/admin.js';
+import authRoutes from './routes/auth.js'
+import staffRoutes from './routes/staff.js';
+import userRoutes from './routes/user.js';
+import { getMetricsSummary } from './controllers/admin.js';
 
 
 const app = express();
@@ -13,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://localhost:5174"],
+        origin: ["https://cms-nine-olive.vercel.app", "http://localhost:5173", "http://localhost:5174"],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization", "userid"],
@@ -23,6 +28,12 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.set("trust proxy", true);
+
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/staff", staffRoutes);
+app.use("/api/user", userRoutes);
+app.get("/api/metrics/summary", getMetricsSummary);
 
 app.use((req, res) => {
     res.status(404).send('404 Not Found');
